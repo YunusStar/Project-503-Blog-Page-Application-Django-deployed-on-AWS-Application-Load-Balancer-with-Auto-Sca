63911793f3bea01de035a17acbebab77d9417774
YunusStar <dolphinstar121@gmail.com>
2021-05-08 20:27:04 +0300

    last time

--- userdata.sh
+++ userdata.sh
@@ -4,7 +4,7 @@ apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
 TOKEN="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"
-git clone $TOKEN@https://github.com/YunusStar/Capstone-Project.git
+git clone https://$TOKEN@github.com/YunusStar/Capstone-Project.git
 cd /home/ubuntu/Capstone-Project
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
