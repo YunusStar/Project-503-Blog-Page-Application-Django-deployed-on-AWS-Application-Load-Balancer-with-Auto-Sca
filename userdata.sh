266e045e3388bb1d56a5a967fe4b43f2e7d2f0a9
YunusStar <dolphinstar121@gmail.com>
2021-05-08 19:44:09 +0300

    changes

--- userdata.sh
+++ userdata.sh
@@ -3,13 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-git clone https://$TOKEN@<YOUR GITHUB REPO URL>
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>
+TOKEN="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"
+git clone https://$TOKEN@https://github.com/YunusStar/Capstone-Project.git
+cd /home/ubuntu/Capstone-Project
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
+cd /home/ubuntu/Capstone-Project/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
